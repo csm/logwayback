a560939c14535189cadd277164a04bb299cb4237
Casey Marshall <casey.s.marshall@gmail.com>
2018-11-01 13:52:20 -0700

    Add release-tasks.

--- project.clj
+++ project.clj
@@ -8,4 +8,14 @@
                  [ch.qos.logback/logback-core "1.1.8"]
                  [ch.qos.logback/logback-classic "1.1.8"]
                  [mvxcvi/puget "1.0.3"]
-                 [environ "1.1.0"]])
+                 [environ "1.1.0"]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version"
+                   "leiningen.release/bump-version" "release"]
+                  ["codox"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag"]
+                  ["deploy" "clojars"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
