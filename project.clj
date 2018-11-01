500ed6eeeff632d8e32743dccc9a741e6f0017b2
Casey Marshall <casey.s.marshall@gmail.com>
2018-11-01 13:59:24 -0700

    Version 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.csm/logwayback "0.1.1"
+(defproject com.github.csm/logwayback "0.1.2-SNAPSHOT"
   :description "Colorful, structured logback output"
   :url "https://github.com/csm/logwayback"
   :license {:name "MIT"
